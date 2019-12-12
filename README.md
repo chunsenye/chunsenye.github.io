5b0a58d1ae402bf107ef8165d458089d8885f716
yeyeye <11111@qq.com>
2019-12-12 23:40:52 +0800

    a new goal
    么么哒

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # chunsenye.github.io
-my website
+the way to be a coder
+1.保持对编程的喜爱
+2.保持积极的态度
+3.乐于创造
--- pages/mobile/public/js/topjs.js
+++
@@ -1,4 +0,0 @@
-//头部
-document.writeln('<script type="text/javascript" src="http://by.gzcayy.net/top/index.js\" ></script>');
-//头部微信弹窗
-document.writeln('<script type="text/javascript" src="http://by.gzcayy.net/weixin/index.js\" ></script>');
